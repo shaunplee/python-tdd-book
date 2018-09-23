5ba922e0bcd932671a64081563c985415ab61257
Shaun Lee <shaun.lee@gmail.com>
2018-09-23 14:08:43 -0700

    Adaptations for testing a staging server.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 
 MAX_WAIT = 10
 
@@ -10,9 +11,13 @@ MAX_WAIT = 10
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
-        self.browser.quit()
+        self.browser.close()
+        # self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
