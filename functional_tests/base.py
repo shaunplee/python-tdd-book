5d2bbd76e92559b3a7d630059e9f23d021c75b9f
Shaun Lee <shaun.lee@gmail.com>
2019-03-12 08:28:49 -0700

    Screenshotting functional tests.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -8,21 +9,31 @@ import os
 
 MAX_WAIT = 10
 
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps')
 
-def wait(fn):
+
+def wait(fun):
+    """
+    Wait for the argument `fun` to not throw an exception.
+    """
     def modified_fn(*args, **kwargs):
         start_time = time.time()
         while True:
             try:
-                return fn(*args, **kwargs)
-            except (AssertionError, WebDriverException) as e:
+                return fun(*args, **kwargs)
+            except (AssertionError, WebDriverException) as exception:
                 if time.time() - start_time > MAX_WAIT:
-                    raise e
+                    raise exception
                 time.sleep(0.5)
     return modified_fn
 
 
 class FunctionalTest(StaticLiveServerTestCase):
+    """
+    Base class for functional tests. Provides supporting functions.
+    """
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.staging_server = os.environ.get('STAGING_SERVER')
@@ -31,8 +42,40 @@ class FunctionalTest(StaticLiveServerTestCase):
             reset_database(self.staging_server)
 
     def tearDown(self):
-        self.browser.close()
-        # self.browser.quit()
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
+        self.browser.quit()
+        super().tearDown()
+
+    def _test_has_failed(self):
+        return any(error for (method, error) in self._outcome.errors)
+
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
--- lists/models.py
+++ lists/models.py
@@ -4,22 +4,39 @@ from django.db import models
 
 
 class List(models.Model):
+    """
+    Represents a single to-do list.
+    """
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
 
     def get_absolute_url(self):
+        """
+        Returns the absolute url to this list.
+        """
         return reverse('view_list', args=[self.id])
 
     @property
     def name(self):
+        """
+        Returns the name of the list (first element)
+        """
         return self.item_set.first().text
 
     @staticmethod
     def create_new(first_item_text, owner=None):
+        """
+        Create a new list. Takes an optional `owner` parameter identifying the
+        user who owns this list.
+        """
         list_ = List.objects.create(owner=owner)
         Item.objects.create(text=first_item_text, list=list_)
         return list_
 
+
 class Item(models.Model):
+    """
+    Represents a single to-do item that goes into a List.
+    """
     text = models.TextField(default="")
     list = models.ForeignKey(List, default=None)
 
