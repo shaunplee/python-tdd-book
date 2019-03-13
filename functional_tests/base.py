0bcf75a5dc3cf08d6d8ac8afd9e56ac367df9d83
Shaun Lee <shaun.lee@gmail.com>
2019-03-13 08:46:57 -0700

    Switch to Chrome for functional tests.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -35,7 +35,7 @@ class FunctionalTest(StaticLiveServerTestCase):
     """
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
